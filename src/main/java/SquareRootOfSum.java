599aa721ee7d3a63830c5aa79d02d637e2f520c6
JuliansRecent <juliansrecent@gmail.com>
2023-02-08 20:18:27 -0800

    Module complete. Create a terminal based application that takes two numbers and gives the square root of the two numbers sum.

--- src/main/java/SquareRootOfSum.java
+++ src/main/java/SquareRootOfSum.java
@@ -5,6 +5,19 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Enter the first integer: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Enter the second integer: ");
+        int secondNumber = scanner.nextInt();
+        // Calculate the sum of the two integers
+        int sum = firstNumber + secondNumber;
+        // Make sure the sum is non-negative
+        if (sum < 0) {
+            System.out.println("The sum is negative. Cannot calculate the square root.");
+        } else {
+            // Calculate the square root of the sum
+            double squareRoot = Math.sqrt(sum);
+            System.out.println("The square root of the sum is: " + squareRoot);
+        }
     }
 }
